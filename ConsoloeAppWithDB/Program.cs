9a1de895d967a535195868d1a9ff6c18fc19b24f
Vadim <vadimdrach600@gmail.com>
2021-04-17 14:43:51 +0300

    Testing Reverse Engineering

--- ConsoloeAppWithDB/Program.cs
+++ ConsoloeAppWithDB/Program.cs
@@ -7,26 +7,40 @@ namespace ConsoloeAppWithDB
     {
         public static void Main(string[] args)
         {
-            using (ApplicationContext db = new ApplicationContext())
+            #region write data
+            //using (ApplicationContext db = new ApplicationContext())
+            //{
+            //    // создаем два объекта User
+            //    User.User user1 = new User.User { name = "Tom", age = 33 };
+            //    User.User user2 = new User.User { name = "Alice", age = 26 };
+
+            //    // добавляем их в бд
+            //    db.Users.Add(user1);
+            //    db.Users.Add(user2);
+            //    db.SaveChanges();
+            //    Console.WriteLine("Объекты успешно сохранены");
+
+            //    // получаем объекты из бд и выводим на консоль
+            //    var users = db.Users.ToList();
+            //    Console.WriteLine("Список объектов:");
+            //    foreach (User.User u in users)
+            //    {
+            //        Console.WriteLine($"{u.Id}.{u.name} - {u.age}");
+            //    }
+            //}
+            #endregion
+
+            using (testappdbContext db = new testappdbContext())
             {
-                // создаем два объекта User
-                User.User user1 = new User.User { name = "Tom", age = 33 };
-                User.User user2 = new User.User { name = "Alice", age = 26 };
-
-                // добавляем их в бд
-                db.Users.Add(user1);
-                db.Users.Add(user2);
-                db.SaveChanges();
-                Console.WriteLine("Объекты успешно сохранены");
-
                 // получаем объекты из бд и выводим на консоль
                 var users = db.Users.ToList();
                 Console.WriteLine("Список объектов:");
-                foreach (User.User u in users)
+                foreach (User u in users)
                 {
-                    Console.WriteLine($"{u.Id}.{u.name} - {u.age}");
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
                 }
             }
+
             Console.ReadKey();
         }
     }
